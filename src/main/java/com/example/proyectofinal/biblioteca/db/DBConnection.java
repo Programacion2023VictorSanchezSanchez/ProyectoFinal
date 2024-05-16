a01f2763133335782fe38e7e5261b64c70ae744c
vagosu <victorss_26@hotmail.com>
2024-05-16 20:08:00 +0200

    Inicio

---
+++ src/main/java/com/example/proyectofinal/biblioteca/db/DBConnection.java
@@ -0,0 +1,49 @@
+package com.example.proyectofinal.biblioteca.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    // URL de conexión a la base de datos MySQL
+    private static final String URL = "jdbc:mysql://localhost:3306/rigemdb";
+    private static final String USERNAME = "usuario";
+    private static final String PASSWORD = "usuario";
+
+    private static Connection connection;
+
+    // Constructor privado para evitar instancias directas
+    private DBConnection() {}
+
+    // Método estático para obtener la instancia única de la conexión
+    public static Connection getConnection() {
+        if (connection == null) {
+            // Bloqueo sincronizado para evitar concurrencia
+            synchronized (DBConnection.class) {
+                if (connection == null) {
+                    try {
+                        // Establecer la conexión
+                        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                    } catch ( SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
+    // Método para cerrar la conexión
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
